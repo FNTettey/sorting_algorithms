8919b50d7dfd1973cd22776a2e66698ac0024bb6
mosweubarona <mosweubarona@gmail.com>
2022-07-17 07:35:42 -0700

    my commit try 2select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,28 +6,29 @@
  * @size: the size of the array
  */
 void selection_sort(int *array, size_t size)
-{
-if (size > 1)
-{
-size_t i, j, smallest;
-int temp;
-for (i = 0; i < size - 1; i++)
-{
-smallest = i;
-for (j = i + 1; j < size; j++)
-{
-if (array[j] < array[smallest])
-{
-smallest = j;
-}
-}
-if (smallest != i)
-{
-temp = array[i];
-array[i] = array[smallest];
-array[smallest] = temp;
-print_array(array, size);
-}
-}
-}
-}
+	{
+		if (size > 1)
+		{
+			size_t i, j, smallest;
+			int temp;
+
+			for (i = 0; i < size - 1; i++)
+			{
+				smallest = i;
+				for (j = i + 1; j < size; j++)
+				{
+					if (array[j] < array[smallest])
+					{
+						smallest = j;
+					}
+				}
+				if (smallest != i)
+				{
+					temp = array[i];
+					array[i] = array[smallest];
+					array[smallest] = temp;
+					print_array(array, size);
+				}
+			}
+		}
+	}
