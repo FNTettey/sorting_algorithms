e147ed99295c83876b36eae2458babe342ead962
mosweubarona <mosweubarona@gmail.com>
2022-07-17 06:59:36 -0700

    my commit try checkr4

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,27 +7,27 @@
  * @size: elements
 */
 void bubble_sort(int *array, size_t size)
-{
-if (size > 1)
-{
-size_t i, len = size;
-short int moved;
-int tmp;
-
-do {
-	moved = 0;
-	for (i = 1; i < size; i++)
 	{
-		if (array[i - 1] > array[i])
+		if (size > 1)
 		{
-			tmp = array[i];
-			array[i] = array[i - 1];
-			array[i - 1] = tmp;
-			moved = i;
-			print_array(array, len);
+			size_t i, len = size;
+			short int moved;
+			int tmp;
+
+			do {
+				moved = 0;
+				for (i = 1; i < size; i++)
+				{
+					if (array[i - 1] > array[i])
+					{
+						tmp = array[i];
+						array[i] = array[i - 1];
+						array[i - 1] = tmp;
+						moved = i;
+						print_array(array, len);
+					}
+				}
+				size = moved;
+			} while (size >= 1);
 		}
 	}
-	size = moved;
-} while (size >= 1);
-}
-}
