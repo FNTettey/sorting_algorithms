b715f817f61daed671b23ad25c7ff57128426233
mosweubarona <mosweubarona@gmail.com>
2022-07-19 07:56:02 -0700

    my commit reparate func 102 count

---
+++ 102-counting_sort-backup.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+/**
+ *  counting_sort- sorts an array of integers
+ *  in ascending order using the counting sort algorithm
+ * @array: a pointer to the array
+ * @size: the size of the array
+ */
+void counting_sort(int *array, size_t size)
+{
+if (size > 1)
+{
+size_t i, max, value;
+int *count, largest;
+int temp[100];
+for (i = 0; i < 100; i++)
+temp[i] = 0;
+/* finding largest value */
+largest = array[0];
+for (i = 1; i < size; i++)
+{
+if (array[i] > largest)
+largest = array[i];
+}
+max = largest;
+/* creating & initialising the counting array */
+count = (int *)malloc(max + 1 * sizeof(int));
+if (count != NULL)
+{
+for (i = 0; i <= max; i++)
+count[i] = 0;
+}
+/* Storing the number of occurances of each element in the count array */
+for (i = 0; i < size; i++)
+count[array[i]]++;
+/* updating the counting array */
+for (i = 0; i <= max; i++)
+count[i] = count[i] + count[i - 1];
+print_array(count, max + 1);
+/* sorting */
+for (i = 0; i < size; i++)
+{
+value = array[i];
+temp[count[value] - 1] = value;
+count[value]++;
+}
+free(count);
+/* putting sorted values in original array */
+for (i = 0; i < size; i++)
+array[i] = temp[i];
+}
+}
--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,51 +1,68 @@
 #include "sort.h"
+
 /**
- *  counting_sort- sorts an array of integers
- *  in ascending order using the counting sort algorithm
- * @array: a pointer to the array
- * @size: the size of the array
+ * get_max - Get the maximum value in an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
+ * counting_sort - Sort an array of integers in ascending order
+ *                 using the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the counting array after setting it up.
  */
 void counting_sort(int *array, size_t size)
 {
-if (size > 1)
-{
-size_t i, max, value;
-int *count, largest;
-int temp[100];
-for (i = 0; i < 100; i++)
-temp[i] = 0;
-/* finding largest value */
-largest = array[0];
-for (i = 1; i < size; i++)
-{
-if (array[i] > largest)
-largest = array[i];
-}
-max = largest;
-/* creating & initialising the counting array */
-count = (int *)malloc(max + 1 * sizeof(int));
-if (count != NULL)
-{
-for (i = 0; i <= max; i++)
-count[i] = 0;
-}
-/* Storing the number of occurances of each element in the count array */
-for (i = 0; i < size; i++)
-count[array[i]]++;
-/* updating the counting array */
-for (i = 0; i <= max; i++)
-count[i] = count[i] + count[i - 1];
-print_array(count, max + 1);
-/* sorting */
-for (i = 0; i < size; i++)
-{
-value = array[i];
-temp[count[value] - 1] = value;
-count[value]++;
-}
-free(count);
-/* putting sorted values in original array */
-for (i = 0; i < size; i++)
-array[i] = temp[i];
-}
+	int *count, *sorted, max, i;
+
+	if (array == NULL || size < 2)
+		return;
+
+	sorted = malloc(sizeof(int) * size);
+	if (sorted == NULL)
+		return;
+	max = get_max(array, size);
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
+	{
+		free(sorted);
+		return;
+	}
+
+	for (i = 0; i < (max + 1); i++)
+		count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count[array[i]] += 1;
+	for (i = 0; i < (max + 1); i++)
+		count[i] += count[i - 1];
+	print_array(count, max + 1);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		sorted[count[array[i]] - 1] = array[i];
+		count[array[i]] -= 1;
+	}
+
+	for (i = 0; i < (int)size; i++)
+		array[i] = sorted[i];
+
+	free(sorted);
+	free(count);
 }
