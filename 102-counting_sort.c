c499931290c3de2128f82d519d07e8dd5d1341a7
=Nii <=nii4tettey@gmail.com>
2022-07-19 04:29:37 +0200

    countins sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -11,8 +11,8 @@ if (size > 1)
 {
 size_t i, max, value;
 int *count, largest;
-int temp[11];
-for (i = 0; i < 11; i++)
+int temp[50];
+for (i = 0; i < 50; i++)
 temp[i] = 0;
 /* finding largest value */
 largest = array[0];
