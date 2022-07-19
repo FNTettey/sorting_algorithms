9d09e53f66f63b15336cd29c5591500f7c8589dc
=Nii <=nii4tettey@gmail.com>
2022-07-19 04:25:49 +0200

    countins sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -7,9 +7,13 @@
  */
 void counting_sort(int *array, size_t size)
 {
+if (size > 1)
+{
 size_t i, max, value;
 int *count, largest;
-int temp[100];
+int temp[11];
+for (i = 0; i < 11; i++)
+temp[i] = 0;
 /* finding largest value */
 largest = array[0];
 for (i = 1; i < size; i++)
@@ -28,21 +32,20 @@ count[i] = 0;
 /* Storing the number of occurances of each element in the count array */
 for (i = 0; i < size; i++)
 count[array[i]]++;
-
 /* updating the counting array */
 for (i = 0; i <= max; i++)
 count[i] = count[i] + count[i - 1];
 print_array(count, max + 1);
-
 /* sorting */
 for (i = 0; i < size; i++)
 {
 value = array[i];
-temp[count[value]] = value;
-count[value] += 1;
+temp[count[value] - 1] = value;
+count[value]++;
 }
 free(count);
 /* putting sorted values in original array */
 for (i = 0; i < size; i++)
-array[i] = temp[i + 1];
+array[i] = temp[i];
+}
 }
