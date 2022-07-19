888f15eeeb3f4fe648b92a26281e0e29d44b56d6
mosweubarona <mosweubarona@gmail.com>
2022-07-19 08:08:41 -0700

    my commit push fixed 102 count

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -4,8 +4,6 @@
  * get_max - Get the maximum value in an array of integers.
  * @array: An array of integers.
  * @size: The size of the array.
- *
- * Return: The maximum integer in the array.
  */
 int get_max(int *array, int size)
 {
@@ -22,14 +20,13 @@ int get_max(int *array, int size)
 
 /**
  * counting_sort - Sort an array of integers in ascending order
- *                 using the counting sort algorithm.
- * @array: An array of integers.
+ * using the counting sort algorithm
+ * @array: a pointer to the array
  * @size: The size of the array.
- *
- * Description: Prints the counting array after setting it up.
  */
 void counting_sort(int *array, size_t size)
 {
+	/* Creating & initialising the counting array */
 	int *count, *sorted, max, i;
 
 	if (array == NULL || size < 2)
@@ -45,20 +42,23 @@ void counting_sort(int *array, size_t size)
 		free(sorted);
 		return;
 	}
+	/* Storing the number of occurences of each element in the count array */
 
 	for (i = 0; i < (max + 1); i++)
 		count[i] = 0;
 	for (i = 0; i < (int)size; i++)
 		count[array[i]] += 1;
+	/* updating the counting array */
 	for (i = 0; i < (max + 1); i++)
 		count[i] += count[i - 1];
 	print_array(count, max + 1);
-
+	/* sorting */
 	for (i = 0; i < (int)size; i++)
 	{
 		sorted[count[array[i]] - 1] = array[i];
 		count[array[i]] -= 1;
 	}
+	/* putting sorted values in original array */
 
 	for (i = 0; i < (int)size; i++)
 		array[i] = sorted[i];
